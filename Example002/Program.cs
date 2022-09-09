4322c5c83d65405785054ab864405b3e21a35502
Ruslan Karimov <ru-ka-@mail.ru>
2022-09-09 20:44:59 +0500

    added example002

---
+++ Example002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double GetDistanceThreePoint(int x1, int y1, int z1, int x2, int y2, int z2) {
+    double distanse = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)  + Math.Pow (z2 - z1, 2));
+    return distanse;
+}
+
+Console.Write("Введите X1: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y1: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Z1: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите X2: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Y2: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+// Расчет расстояния между двумя точками на плоскости
+double result = GetDistanceThreePoint(x1, y1, z1, x2, y2, z2);
+Console.WriteLine($"Расстояние между точками ({x1}, {y1}, {z1}) и ({x2}, {y2}, {z2}) равно {Math.Round(result, 2, MidpointRounding.ToNegativeInfinity)}");
