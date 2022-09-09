1064cf0d3dc8a4942ae167de12eb602d9c37210e
Ruslan Karimov <ru-ka-@mail.ru>
2022-09-09 20:43:12 +0500

    added example001

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+string x = Console.ReadLine();
+
+void searhPolindrome(string x){
+ if (x[0]==x[4] || x[1]==x[3]){
+ Console.WriteLine($"Число {x} - палиндром");
+  }
+ else Console.WriteLine($"Число {x} - не палиндром");
+}
+
+if (x.Length == 5){
+ searhPolindrome(x);
+}
+else Console.WriteLine($"Введите положительное целое пятизначное число");
